6ba1c4c14afdb8b71b57e3e4ff779e5e8d78ce9e
abhishekY2401 <abhiyd2401@gmail.com>
2024-10-04 01:17:05 +0530

    setup flask and postgres db for orders

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    DB_PASSWORD = os.environ['DB_PASSWORD']
+    RABBITMQ_URL = os.environ['RABBITMQ_URI']
+    SQLALCHEMY_DATABASE_URI = (
+        f'postgresql://postgres:{DB_PASSWORD}@localhost:5432/orders_db'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+    ORDER_PLACED_QUEUE = "order_placed"
+    ORDER_SHIPPED_QUEUE = "order_shipped"
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
