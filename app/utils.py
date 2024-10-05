056906792daf8bcc866bde73d64c7838aa6a10e6
abhishekY2401 <abhiyd2401@gmail.com>
2024-10-05 09:00:30 +0530

    pass auth token for querying prices

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,7 @@ import requests
 PRODUCT_MICROSERVICE_URL = "http://127.0.0.1:7000/graphql"
 
 
-def fetch_product_prices(product_ids):
+def fetch_product_prices(product_ids, token):
     query = """
         query getProductByIds($ids: [ID!]!) {
             productByIds(ids: $ids) {
@@ -15,11 +15,19 @@ def fetch_product_prices(product_ids):
 
     variables = {"ids": product_ids}
 
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
     response = requests.post(
         f"{PRODUCT_MICROSERVICE_URL}",
-        json={"query": query, "variables": variables}
+        json={"query": query, "variables": variables},
+        headers=headers
     )
 
-    data = response.json()['data']['productByIds']
+    if response.status_code != 200:
+        raise Exception(f"Error fetching product prices: {response.text}")
+
+    data = response.json().get('data', {}).get('productByIds', [])
 
     return {product["id"]: product["price"] for product in data}
